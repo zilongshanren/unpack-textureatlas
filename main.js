a61336fe57284d3cb71932e79f6398a33b455e0e
zilongshanren <guanghui8827@126.com>
2017-01-06 18:10:10 +0800

    make sharp works

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
 'use strict';
+const Fs = require('fire-fs');
+const Async = require('async');
+const Os = require('os');
+const Path = require('fire-path');
+var sharpPath;
+if (Editor.dev) {
+  sharpPath = 'sharp';
+} else {
+  sharpPath = Editor.url('unpack://utils/sharp');
+}
+const Sharp = require(sharpPath);
 
 module.exports = {
   load () {
@@ -18,7 +29,18 @@ module.exports = {
         let selectionUUid = currentSelection[0];
         let selectionMeta = Editor.assetdb.loadMetaByUuid(selectionUUid);
         let assetInfo = Editor.assetdb.assetInfoByUuid(selectionUUid);
-        if (assetInfo.type === "sprite-atlas" && selectionMeta.type === "Texture Packer") {
+        var textureAtlasPath = Editor.assetdb.uuidToFspath(selectionMeta.rawTextureUuid);
+        var textureAtlasBaseName = Path.dirname(textureAtlasPath);
+        var textureAtlasSubMetas = selectionMeta.getSubMetas();
+      
+        if (assetInfo.type === "sprite-atlas" 
+            && selectionMeta.type === "Texture Packer"
+           && textureAtlasSubMetas) {
+
+            Sharp(textureAtlasPath).extract({left: 0, top: 0, width: 100, height:100}).toFile(textureAtlasBaseName + "/test.png", (err) => {
+          if (err) Editor.error(err);
+        });
+
           Editor.log("selection texture packer asset.");
         } else {
           Editor.Dialog.messageBox({
