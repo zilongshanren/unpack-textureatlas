6f89ac5cf370560cdac0b232ef6a02c5a312698a
zilongshanren <guanghui8827@126.com>
2017-01-06 17:30:40 +0800

    handle selected asset

--- main.js
+++ main.js
@@ -11,17 +11,36 @@ module.exports = {
 
   // register your ipc messages here
   messages: {
-    'open' () {
-      // open entry panel registered in package.json
-      Editor.Panel.open('unpack-textureatlas');
+    'unpack' () {
+      let currentSelection = Editor.Selection.curSelection('asset');
+      if (currentSelection.length > 0) {
+        let selectionUUid = currentSelection[0];
+        let selectionMeta = Editor.assetdb.loadMetaByUuid(selectionUUid);
+        let assetInfo = Editor.assetdb.assetInfoByUuid(selectionUUid);
+        if (assetInfo.type === "sprite-atlas" && selectionMeta.type === "Texture Packer") {
+          Editor.log("selection texture packer asset.");
+        } else {
+          Editor.Dialog.messageBox({
+            type: 'warning',
+            buttons: ['OK'],
+            titile: 'Unpack Texture Packer Atlas',
+            message: 'The selected asset is not a Texture Packer asset!',
+            defaultId: 0,
+            noLink: true
+          });
+        }
+      } else {
+         Editor.Dialog.messageBox({
+            type: 'warning',
+            buttons: ['OK'],
+            titile: 'Unpack Texture Packer Atlas',
+            message: 'Please select a Texture Packer asset at first!',
+            defaultId: 0,
+            noLink: true
+          });
+      }
+      
+     
     },
-    'say-hello' () {
-      Editor.log('Hello World!');
-      // send ipc message to panel
-      Editor.Ipc.sendToPanel('unpack-textureatlas', 'unpack-textureatlas:hello');
-    },
-    'clicked' () {
-      Editor.log('Button clicked!');
-    }
   },
 };
