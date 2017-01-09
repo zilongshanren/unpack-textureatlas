ca3cb09d0b3e51f101b53c1dc20ec5811e760b04
zilongshanren <guanghui8827@126.com>
2017-01-09 14:13:57 +0800

    use async

--- main.js
+++ main.js
@@ -41,48 +41,36 @@ module.exports = {
           Fs.mkdirsSync(extractImageSavePath);
 
           var spriteFrameNames = Object.keys(textureAtlasSubMetas);
-          Editor.log("Sprite frame names" + spriteFrameNames);
-          var functionList = [];
-          spriteFrameNames.forEach(function (spriteFrameName) {
-            var next = () => {
-              var spriteFrameObj = textureAtlasSubMetas[spriteFrameName];
-              var isRotated = spriteFrameObj.rotated;
-              var originalSize = cc.size(spriteFrameObj.rawWidth, spriteFrameObj.rawHeight);
-              var rect = cc.rect(spriteFrameObj.trimX, spriteFrameObj.trimY, spriteFrameObj.width,spriteFrameObj.height);
-              var offset = cc.p(spriteFrameObj.offsetX, spriteFrameObj.offsetY);
-              var trimmedLeft = offset.x + (originalSize.width - rect.width) / 2;
-              var trimmedRight = offset.x - (originalSize.width - rect.width) / 2;
-              var trimmedTop = offset.y - (originalSize.height - rect.height) / 2;
-              var trimmedBottom = offset.y + (originalSize.height - rect.height) / 2;
+          Async.forEach(spriteFrameNames, function (spriteFrameName, next) {
+            var spriteFrameObj = textureAtlasSubMetas[spriteFrameName];
+            var isRotated = spriteFrameObj.rotated;
+            var originalSize = cc.size(spriteFrameObj.rawWidth, spriteFrameObj.rawHeight);
+            var rect = cc.rect(spriteFrameObj.trimX, spriteFrameObj.trimY, spriteFrameObj.width,spriteFrameObj.height);
+            var offset = cc.p(spriteFrameObj.offsetX, spriteFrameObj.offsetY);
+            var trimmedLeft = offset.x + (originalSize.width - rect.width) / 2;
+            var trimmedRight = offset.x - (originalSize.width - rect.width) / 2;
+            var trimmedTop = offset.y - (originalSize.height - rect.height) / 2;
+            var trimmedBottom = offset.y + (originalSize.height - rect.height) / 2;
 
-              var ss = Sharp(textureAtlasPath);
-              var sharpOptioins = ss.extract({left: rect.x, top: rect.y, width: rect.width, height:rect.height})
-                  .resize(originalSize.width, originalSize.height)
-                  .background({r: 0, g: 0, b: 0, alpha: 0})
-                  .extend({top: trimmedTop, bottom: trimmedBottom, left: trimmedLeft, right: trimmedRight});
+            var ss = Sharp(textureAtlasPath);
+            var sharpOptioins = ss.extract({left: rect.x, top: rect.y, width: rect.width, height:rect.height})
+                .resize(originalSize.width, originalSize.height)
+                .background({r: 0, g: 0, b: 0, alpha: 0})
+                .extend({top: trimmedTop, bottom: trimmedBottom, left: trimmedLeft, right: trimmedRight});
 
-              if (isRotated) {
-                sharpOptioins.rotate(270);
-              } else {
-                sharpOptioins.rotate(0);
-              }
+            if (isRotated) {
+              sharpOptioins.rotate(270);
+            } else {
+              sharpOptioins.rotate(0);
+            }
 
 
-              sharpOptioins.toFile(extractImageSavePath + spriteFrameName, (err) => {
-                if (err) Editor.error(err);
+            sharpOptioins.toFile(extractImageSavePath + spriteFrameName, (err) => {
+              if (err) Editor.error("Generating " + spriteFrameName + ", " + err);
 
-                Editor.log(spriteFrameName + " is generated.");
-              });
-            };
-
-            functionList.push(next);
-          });
-          Editor.log(functionList);
-
-          Async.series(functionList, (err) => {
-            if (err) Editor.error(err);
-
-            Editor.log("All textures have been extracted.");
+              Editor.log(spriteFrameName + " is generated.");
+              next();
+            });
           });
 
         } else {
