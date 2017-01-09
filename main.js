812ff3eed259bdde14494d43c0d407daa440f0a6
zilongshanren <guanghui8827@126.com>
2017-01-09 14:24:31 +0800

    finish

--- main.js
+++ main.js
@@ -52,25 +52,40 @@ module.exports = {
             var trimmedTop = offset.y - (originalSize.height - rect.height) / 2;
             var trimmedBottom = offset.y + (originalSize.height - rect.height) / 2;
 
-            var ss = Sharp(textureAtlasPath);
-            var sharpOptioins = ss.extract({left: rect.x, top: rect.y, width: rect.width, height:rect.height})
+            if (isRotated) {
+              Sharp(textureAtlasPath).extract({left: rect.x, top: rect.y, width: rect.height, height:rect.width})
                 .resize(originalSize.width, originalSize.height)
                 .background({r: 0, g: 0, b: 0, alpha: 0})
-                .extend({top: trimmedTop, bottom: trimmedBottom, left: trimmedLeft, right: trimmedRight});
+                .extend({top: trimmedTop, bottom: trimmedBottom, left: trimmedLeft, right: trimmedRight})
+                .rotate(270)
+                .toFile(extractImageSavePath + spriteFrameName, (err) => {
+                  if (err) {
+                    Editor.error("Generating " + spriteFrameName + ", " + err);
+                    Editor.error({left: rect.x, top: rect.y, width: rect.width, height:rect.height});
+                  }
 
-            if (isRotated) {
-              sharpOptioins.rotate(270);
+                  Editor.log(spriteFrameName + " is generated successfully!");
+                  next();
+                });
+
             } else {
-              sharpOptioins.rotate(0);
+              Sharp(textureAtlasPath).extract({left: rect.x, top: rect.y, width: rect.width, height:rect.height})
+                .resize(originalSize.width, originalSize.height)
+                .background({r: 0, g: 0, b: 0, alpha: 0})
+                .extend({top: trimmedTop, bottom: trimmedBottom, left: trimmedLeft, right: trimmedRight})
+                .rotate(0)
+                .toFile(extractImageSavePath + spriteFrameName, (err) => {
+                  if (err) {
+                    Editor.error("Generating " + spriteFrameName + ", " + err);
+                    Editor.error({left: rect.x, top: rect.y, width: rect.width, height:rect.height});
+                  }
+
+                  Editor.log(spriteFrameName + " is generated successfully!");
+                  next();
+                });
             }
-
-
-            sharpOptioins.toFile(extractImageSavePath + spriteFrameName, (err) => {
-              if (err) Editor.error("Generating " + spriteFrameName + ", " + err);
-
-              Editor.log(spriteFrameName + " is generated.");
-              next();
-            });
+          }, () => {
+            Editor.log(`There are ${spriteFrameNames.length} textures are generated!`);
           });
 
         } else {
